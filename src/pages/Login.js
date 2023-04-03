b6ca95857bafbc4a1fe57a42c68abfd597c8b462
RaphaelMocellin <raphael.mocellin@gmail.com>
2023-04-03 16:03:54 -0300

    Elaborados os reqs 2 e 3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import ReceitasContext from '../context/ReceitasContext';
 
 function Login() {
@@ -6,9 +6,40 @@ function Login() {
   const { teste } = RContext;
   console.log(teste);
 
+  const [email, setEmail] = useState('');
+  const [password, setPassord] = useState('');
+
   return (
     <div>
       <h1>Login</h1>
+      <label htmlFor="emailInput">
+        Email
+        <input
+          id="emailInput"
+          type="email"
+          value={ email }
+          onChange={ (e) => setEmail(e.target.value) }
+          data-testid="email-input"
+        />
+      </label>
+      <label htmlFor="passwordInput">
+        Senha
+        <input
+          id="passwordInput"
+          type="password"
+          value={ password }
+          onChange={ (e) => setPassord(e.target.value) }
+          data-testid="password-input"
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        onClick={ () => console.log('ok') }
+      >
+        Login
+
+      </button>
     </div>
   );
 }
