b23d325e89b6e8a8e2719d9887c17b50cafdeef2
wladyrjusta <wladyr.justa@gmail.com>
2023-04-04 13:51:30 -0300

    Criado footer com icones de direcionamentos para páginas /meals e /drinks, componente renderizado apenas em Meals, Profile e Drinks. Criados testes de cobertura para o componente Footer.

--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -1,11 +1,17 @@
 import React from 'react';
+
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Meals() {
   return (
-    <div>
-      <Header title="Meals" search />
-    </div>
+    <>
+      <div>
+        <Header title="Meals" search />
+      </div>
+
+      <Footer />
+    </>
   );
 }
 
--- src/pages/Profle.js
+++ src/pages/Profle.js
@@ -1,11 +1,17 @@
 import React from 'react';
+
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Profile() {
   return (
-    <div>
-      <Header title="Profile" search={ false } />
-    </div>
+    <>
+      <div>
+        <Header title="Profile" search={ false } />
+      </div>
+      <Footer />
+
+    </>
   );
 }
 
